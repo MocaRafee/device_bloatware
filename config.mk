e0c21c62c75f38828d1223781248812d63dfa728
Mocarafee <harif.rp05@gmail.com>
2021-01-28 14:56:21 +0700

    bloatware: use RetroMusic instead
    BeatPlayer UI still doesn't adaptable to big display size

--- config.mk
+++ config.mk
@@ -35,9 +35,7 @@ PRODUCT_PACKAGES += \
 # My Preffered Bloated Apps
 PRODUCT_PACKAGES += \
     GalleryGo \
-    KimchiRecorder \
-    BeatPlayer
-##    Musicolet 
+    KimchiRecorder
 	
 # Wallpaper apps like AOSPA ones, enable it if you like AOSPA/Oneplus styled wallpaper
 #PRODUCT_PACKAGES += \
