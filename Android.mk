900f7df8ff76d3654801ad952b8bdd05146e8224
Mocarafee <harif.rp05@gmail.com>
2021-10-19 14:25:06 +0700

    bloatware: Disable RemovePackage

--- Android.mk
+++ Android.mk
@@ -119,15 +119,15 @@ LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_MODULE_PATH := $(TARGET_OUT)/priv-app
 include $(BUILD_PREBUILT)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE := RemovePackages
-LOCAL_MODULE_CLASS := APPS
-LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := AudioFX Jelly GoogleTTS talkback PixelLiveWallpaperPrebuilt ChromeModernPublic RetroMusicPlayerPrebuilt RetroMP Camera2
-LOCAL_UNINSTALLABLE_MODULE := true
-LOCAL_CERTIFICATE := PRESIGNED
-LOCAL_SRC_FILES := /dev/null
-include $(BUILD_PREBUILT)
+#include $(CLEAR_VARS)
+#LOCAL_MODULE := RemovePackages
+#LOCAL_MODULE_CLASS := APPS
+#LOCAL_MODULE_TAGS := optional
+#LOCAL_OVERRIDES_PACKAGES := AudioFX Jelly GoogleTTS talkback PixelLiveWallpaperPrebuilt ChromeModernPublic RetroMusicPlayerPrebuilt RetroMP Camera2
+#LOCAL_UNINSTALLABLE_MODULE := true
+#LOCAL_CERTIFICATE := PRESIGNED
+#LOCAL_SRC_FILES := /dev/null
+#include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE        := RetroMP
